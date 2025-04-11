375f95aba7d200e3775976d8ce949cb920511d49
Jiren0 <adeildodeido9@gmail.com>
2025-04-11 07:55:18 -0300

    Add UI

--- ola.java
+++ ola.java
@@ -1,7 +1,8 @@
+import javax.swing.JOptionPane;
 public class ola {
 
     public static void main(String[] args){
-        System.out.println("Antunes GOAT");
+        //System.out.println("Antunes GOAT");
+        JOptionPane.showMessageDialog(null, "Antunes GOAT");
     }
-
 }
